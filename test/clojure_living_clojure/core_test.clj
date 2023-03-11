3e5ee4bf25f8e1cd5c600cc5f54fd2b3cee9eefb
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2023-03-11 19:57:34 +0700

    initial commit

---
+++ test/clojure_living_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-living-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-living-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
